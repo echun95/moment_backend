4c4c957511eab1e38f123a005ee09aecde93d354
leeyoungchun <dldydcns123@gmail.com>
2024-06-13 23:07:31 +0900

    feat: 프로젝트 구조 설계
    1. multi module 생성
    2. 개발환경 설정

---
+++ server-api/src/main/java/com/moment/test/controller/testController.java
@@ -0,0 +1,17 @@
+package com.moment.test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class testController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<>("test success!", HttpStatus.OK);
+    }
+}
---
+++ server-api/src/test/java/com/moment/test/controller/testControllerTest.java
@@ -0,0 +1,15 @@
+package com.moment.test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class testControllerTest {
+
+    @Test
+    void test1(){
+        System.out.println("test!");
+    }
+}
