93dc86fcce034c81898f2bb78cbd3746a1d9281f
leeyoungchun <dldydcns123@gmail.com>
2024-07-07 18:53:17 +0900

    feat: swagger 설정

--- server-api/src/main/java/com/moment/test/controller/testController.java
+++ server-api/src/main/java/com/moment/test/controller/testController.java
@@ -2,6 +2,7 @@ package com.moment.test.controller;
 
 import com.moment.common.exception.RestApiException;
 import com.moment.common.exception.test.TestErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
 
     @GetMapping("/test")
+    @Operation(summary = "test")
     public ResponseEntity<String> test(){
         return new ResponseEntity<>("test success!", HttpStatus.OK);
     }
     @GetMapping("/test1")
+    @Operation(summary = "custom err test")
     public ResponseEntity<String> test1(){
         throw new RestApiException(TestErrorCode.TEST_ERROR_1);
     }
     @GetMapping("/test2")
+    @Operation(summary = "runtime err test")
     public ResponseEntity<String> test2(){
         throw new RuntimeException("test npe");
     }
